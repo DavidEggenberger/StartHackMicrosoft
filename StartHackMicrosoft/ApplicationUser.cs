52bc297123d71a078a7d1dbc9782c88c1acff439
DavidEggenberger <david99.eggenberger@gmail.com>
2022-03-23 23:17:10 +0100

    Domain init

---
+++ StartHackMicrosoft/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public bool PrivateProfile { get; set; }
+        public int TabsOpen { get; set; }
+        public bool IsOnline { get; set; }
+        public string Motto { get; set; }
+    }
+}
