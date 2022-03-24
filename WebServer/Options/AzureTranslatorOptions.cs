661f65f68313893aee4b532e489f64f19063a7e9
DavidEggenberger <david99.eggenberger@gmail.com>
2022-03-24 09:19:11 +0100

    LoadingScreen

--- WebServer/Options/AzureTranslatorOptions.cs
+++ WebServer/Options/AzureTranslatorOptions.cs
@@ -3,6 +3,7 @@
     public class AzureTranslatorOptions
     {
         public string Endpoint { get; set; }
+        public string Location { get; set; }
         public string APIKey { get; set; }
     }
 }
--- WebServer/Services/TranslatorService.cs
+++ WebServer/Services/TranslatorService.cs
@@ -5,32 +5,32 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace WebServer.Services
 {
     public class TranslatorService
     {
-        private readonly IOptions<AzureTranslatorOptions> azureTranslatorOptions;
+        private readonly AzureTranslatorOptions azureTranslatorOptions;
         public TranslatorService(IOptions<AzureTranslatorOptions> azureTranslatorOptions)
         {
-            this.azureTranslatorOptions = azureTranslatorOptions;
+            this.azureTranslatorOptions = azureTranslatorOptions.Value;
         }
 
-        public async Task<string> TranslateToEnglisch(string message)
+        public async Task<string> TranslateToEnglisch(string textToTranslate)
         {
             string route = "/translate?api-version=3.0&from=en&to=de&to=it";
-            string textToTranslate = "Hello, world!";
             object[] body = new object[] { new { Text = textToTranslate } };
-            var requestBody = JsonConverter.SerializeObject(body);
+            var requestBody = JsonSerializer.Serialize(body);
 
             using (var client = new HttpClient())
             using (var request = new HttpRequestMessage())
             {
                 request.Method = HttpMethod.Post;
-                request.RequestUri = new Uri(endpoint + route);
-                request.Content = new StringContent(requestBody, Encoding.UTF8 "application/json");
-                request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
-                request.Headers.Add("Ocp-Apim-Subscription-Region", location);
+                request.RequestUri = new Uri(azureTranslatorOptions.Endpoint + route);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", azureTranslatorOptions.APIKey);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", azureTranslatorOptions.Location);
 
                 HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
                 return await response.Content.ReadAsStringAsync();
